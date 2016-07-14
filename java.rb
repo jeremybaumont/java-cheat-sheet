8768f11cfd1e1067b69fd99b5db84b2e17036fb5
Lorin Hochstein <lhochstein@netflix.com>
2016-07-14 09:13:55 -0700

    initial commit

---
+++ java.rb
@@ -0,0 +1,18 @@
+cheatsheet do
+    title 'Java'
+    docset_file_name 'Java'
+    keyword 'java'
+    category do
+        id 'RxJava'
+        entry do
+            name 'Observable to list'
+            notes <<-'END'
+            ```
+            Observable<T> obs = ...;
+            List<T> l = obs.toList().blocking().single();
+            ```
+            END
+        end
+    end
+end
+
