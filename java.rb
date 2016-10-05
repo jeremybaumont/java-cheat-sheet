4eecce6f7b18fd56f47531b3fd27cbd36bf9e736
Lorin Hochstein <lhochstein@netflix.com>
2016-10-05 10:45:12 -0700

    update

--- java.rb
+++ java.rb
@@ -7,7 +7,7 @@ cheatsheet do
         entry do
             name 'Observable to list'
             notes <<-'END'
-            ```
+            ```java
             Observable<T> obs = ...;
             List<T> l = obs.toList().toBlocking().single();
             ```
@@ -19,7 +19,7 @@ cheatsheet do
         entry do
             name 'List of maps to single map, clobbering duplicate keys'
             notes <<-'END'
-            ```
+            ```java
             List<Map<String, Long>> input = ...;
             output = input.stream().reduce(new HashMap<>(), (x, y) -> {
                 Map<String, Long> result = new HashMap<>(x);
@@ -36,12 +36,35 @@ cheatsheet do
         entry do
             name 'Create mock object with method'
             notes <<-'END'
-            ```
+            ```java
             import static org.mockito.Mockito.mock;
             import static org.mockito.Mockito.when;
 
             MyClass x = mock(MyClass.class);
             when(x.myFunc()).thenReturn("dummy value");
+            ```
+            END
+        end
+        entry do
+            name 'Match any argument'
+            notes <<-'END'
+            ```java
+
+            import static org.mockito.Mockito.mock;
+            import static org.mockito.Matchers.any;
+
+            when(x.foo(any())).thenReturn(12);
+
+            ```
+            END
+        end
+        entry do
+            name 'Unmock a single method'
+            notes <<-'END'
+            ```java
+            import static org.mockito.Mockito.mock;
+
+            when(mock.bar()).thenCallRealMethod();
             ```
             END
         end
